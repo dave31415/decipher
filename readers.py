5a65d93ad5394541d600e9ba3e3683e1d6baaa5b
David Johnston <dave31415@gmail.com>
2015-09-14 16:05:38 -0400

    tests for readers, alphabet file, reader of corpus now generator

---
+++ readers.py
@@ -0,0 +1,23 @@
+data_dir = 'data'
+from alphabet import alphabet
+
+
+def read_encoded_text():
+    """
+    :return: full string of the encoded text file
+    """
+    filename = "encoded-en.txt"
+    return open("%s/%s" % (data_dir, filename), 'rU').read().lower()
+
+
+def read_corpus_line_by_line():
+    """
+    :return: generator to lines of corpus text
+    """
+    filename = "corpus-en.txt"
+    with open("%s/%s" % (data_dir, filename), 'rU') as file:
+        for line in file:
+            yield line.strip().lower()
+
+
+
---
+++ test/test_readers.py
@@ -0,0 +1,33 @@
+import unittest
+from unittest import TestCase
+from readers import read_encoded_text, read_corpus_line_by_line
+
+
+class TestReadEncodedText(TestCase):
+    def setUp(self):
+        self.text = read_encoded_text()
+
+    def test_length_is_correct(self):
+        self.assertEquals(len(self.text), 810)
+
+    def test_starts_with_expected_letters(self):
+        start = "uml pknz nx njl"
+        self.assertTrue(self.text.startswith(start))
+
+
+class TestReadCorpus(TestCase):
+    def setUp(self):
+        self.stream = read_corpus_line_by_line()
+
+    def test_content_of_first_three_lines(self):
+        line = self.stream.next()
+        self.assertTrue(line.endswith('carroll'))
+        line = self.stream.next()
+        self.assertEquals(line, '')
+        expected = 'this ebook is for the use of anyone anywhere at no cost and with'
+        line = self.stream.next()
+        self.assertEquals(line, expected)
+
+
+if __name__ == "__main__":
+    unittest.main()
