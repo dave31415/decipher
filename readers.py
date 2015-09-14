7b3ac97b05607624b5eb6af68bd14b5b899e3507
David Johnston <dave31415@gmail.com>
2015-09-14 16:07:42 -0400

    comments

--- readers.py
+++ readers.py
@@ -4,7 +4,7 @@ from alphabet import alphabet
 
 def read_encoded_text():
     """
-    :return: full string of the encoded text file
+    :return: full string of the encoded text file, lowercase
     """
     filename = "encoded-en.txt"
     return open("%s/%s" % (data_dir, filename), 'rU').read().lower()
@@ -12,7 +12,8 @@ def read_encoded_text():
 
 def read_corpus_line_by_line():
     """
-    :return: generator to lines of corpus text
+    :return: generator to lines of corpus text, stripped of
+             endlines and lowercased
     """
     filename = "corpus-en.txt"
     with open("%s/%s" % (data_dir, filename), 'rU') as file:
