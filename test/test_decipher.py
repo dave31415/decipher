9d8d7d3cd5ccf95e7807dfee8bfd1a72e96fce53
David Johnston <dave31415@gmail.com>
2015-09-15 14:18:17 -0400

    test_decipher added

---
+++ test/test_decipher.py
@@ -0,0 +1,13 @@
+import unittest
+from unittest import TestCase
+
+#TODO: add real tests
+
+
+class TestNothingAgain(TestCase):
+    def test_nothing(self):
+        assert True
+
+
+if __name__ == "__main__":
+    unittest.main()
